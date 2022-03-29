132ff776dcbb53a3af0d017a32c7f54277ad4810
olgaGor03 <92055119+olgaGor03@users.noreply.github.com>
2022-03-29 15:16:07 +0000

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(){
- std::string name; // тип<переменная>
- std::cin >> name; // entering имя.
+ std::string name; // string variable
+ std::cin >> name; // entering name
  std::cout << "Hello world from "<< name << std::endl; // output
 }
