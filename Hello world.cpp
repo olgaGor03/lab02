6c6095c1c0d312a1cf386d732af33bcf0295d2ce
olgaGor03 <olga20031506@gmail.com>
2022-03-29 21:08:00 +0300

    Comments

--- Hello world.cpp
+++ Hello world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(){
- std::string name;
- std::cin >> name;
- std::cout << "Hello world from "<< name << std::endl;
+ std::string name; // string variable
+ std::cin >> name; // entering name
+ std::cout << "Hello world from "<< name << std::endl; // output
 }
